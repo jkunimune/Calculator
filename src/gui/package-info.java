cc17e0e7724c6b0e37ab2870d7e13c99f037c96c
jkunimune <jkunimune@522MT32.olin.edu>
2016-09-10 17:06:33 -0400

    The Patented Non-Terrible Calculator App!
    I created the base structure for my calculator app. It has two packages.
    There will be classes in them, soon.

---
+++ src/gui/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains all classes pertaining to user interface, including
+ * all JavaFM components, plots, and GUIs.
+ */
+/**
+ * @author jkunimune
+ */
+package gui;
---
+++ src/maths/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains all calculation classes, including things like
+ * Expressions, Vectors, etc.
+ */
+/**
+ * @author jkunimune
+ */
+package maths;
